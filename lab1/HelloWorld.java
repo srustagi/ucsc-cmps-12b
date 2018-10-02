758e989c71972b9178dd587701894f6ee1bad0ba
Shivansh Rustagi <shivrustagi221@gmail.com>
2018-10-02 11:52:50 -0700

    initial commit

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,10 @@
+//-----------------------------------------------------------------
+// HelloWorld.java
+// Prints "Hello, World", the first program you should always write
+// ----------------------------------------------------------------
+
+class HelloWorld{
+	static public void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
