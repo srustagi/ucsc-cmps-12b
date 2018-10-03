ad05e466f745af2681ace2a1236b1e30e83aefa9
Shivansh Rustagi <shivrustagi221@gmail.com>
2018-10-02 18:13:37 -0700

    add new makefiles and add HelloUser2

---
+++ lab1/HelloUser2.java
@@ -0,0 +1,8 @@
+class HelloUser2{
+	static public void main(String[] args){
+		System.out.println("Har ghadi badal raha hai roop zindagi");
+		System.out.println("Chaav hai kahhi hai dhoop zidnagi");
+		System.out.println("Har pal yahan jee bhar jiyo");
+		System.out.println("Jo hai sama, kal ho na ho");
+	}
+}
